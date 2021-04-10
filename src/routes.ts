a239de6b8e40069350efaf468ba655db02398944
Carlos Miguel <carlos.miguel.oliveira.17@gmail.com>
2021-04-10 07:55:38 -0300

    adding: router file

--- src/routes.ts
+++ src/routes.ts
@@ -1,33 +1,69 @@
-import {
-  FiHome,
-  FiHelpCircle,
-  FiAward,
-  FiBook,
-  FiFeather
-} from 'react-icons/fi'
+import home from '@/assets/icons/home.svg'
+import trending from '@/assets/icons/trending.svg'
+import streaming from '@/assets/icons/streaming.svg'
+import playlist from '@/assets/icons/playlist.svg'
+import bookmark from '@/assets/icons/bookmark.svg'
 
-import { IoMdHelp } from 'react-icons/io'
+import liveStream from '@/assets/icons/liveStream.svg'
+import tutorial from '@/assets/icons/tutorial.svg'
+import competition from '@/assets/icons/competition.svg'
+import community from '@/assets/icons/community.svg'
 
 const routes = [
   {
-    route: '/',
-    icon: FiHome
+    title: 'MENU',
+    routes: [
+      {
+        path: '/',
+        name: 'Discover',
+        icon: home
+      },
+      {
+        path: '/#',
+        name: 'Trending',
+        icon: trending
+      },
+      {
+        path: '/#',
+        name: 'Streaming',
+        icon: streaming
+      },
+      {
+        path: '/#',
+        name: 'Playlist',
+        icon: playlist
+      },
+      {
+        path: '/#',
+        name: 'Bookmark',
+        icon: bookmark
+      }
+    ]
   },
   {
-    route: '/about',
-    icon: FiHelpCircle
-  },
-  {
-    route: '/#2',
-    icon: FiAward
-  },
-  {
-    route: '/projects',
-    icon: FiBook
-  },
-  {
-    route: '/proposal',
-    icon: FiFeather
+    title: 'CATEGORY',
+    routes: [
+      {
+        path: '/#',
+        name: 'Live Stream',
+        icon: liveStream
+      },
+      {
+        path: '/#',
+        name: 'Tutorial',
+        icon: tutorial
+      },
+      {
+        path: '/#',
+        name: 'Competition',
+        icon: competition
+      },
+      {
+        path: '/#',
+        name: 'Community',
+        icon: community
+      }
+    ]
   }
 ]
 
